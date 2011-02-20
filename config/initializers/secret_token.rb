3489670b377f8fa38b2c2be1949892d079ccd431
agarg <anshul.garg@gmail.com>
2011-02-20 10:58:31 -0600

    Initial Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectV::Application.config.secret_token = '8b280a26318ec63be520e59b62362bf0cfc49edae0d9e4fbe45f33fc42dd5b7a055f72bb7edfbb037e270f3395d7a03c5da8979511b15941ea9e53255677a994'
